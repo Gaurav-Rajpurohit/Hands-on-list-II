825bca492eeacbbedabf4d0b39f7827b8a312f68
Gaurav Rajpurohit <1762003rajpurohitgaurav@gmail.com>
2025-09-21 15:59:46 +0530

    Third commit

---
+++ que31/31b.c
@@ -0,0 +1,45 @@
+/*
+============================================================================
+Name : 31b.c
+Author : Gaurav Rajpurohit
+Description : b. Create a counting semaphore 
+Date: Sept st, 2025.
+============================================================================
+*/
+
+
+
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/sem.h>
+#include<stdlib.h>
+
+int main(){
+	int key, semid;
+
+	key = ftok(".", 'c');
+
+	semid = semget(key, 1, 0666 | IPC_CREAT);
+
+	semctl(semid, 0, SETVAL, 5);
+
+	return 0;
+}
+/*OUTPUT
+gaurav176@gaurav176-HP-Pavilion-Laptop-14-dv0xxx:~/Desktop/handsonlist2/que31$ ipcs -s
+
+------ Semaphore Arrays --------
+key        semid      owner      perms      nsems     
+0x6706729f 0          gaurav176  666        1         
+0x6306729f 1          gaurav176  666        1         
+
+gaurav176@gaurav176-HP-Pavilion-Laptop-14-dv0xxx:~/Desktop/handsonlist2/que31$ ipcs -s -i 1
+
+Semaphore Array semid=1
+uid=1000	 gid=1000	 cuid=1000	 cgid=1000
+mode=0666, access_perms=0666
+nsems = 1
+otime = Not set                   
+ctime = Sun Sep 21 15:54:34 2025  
+semnum     value      ncount     zcount     pid       
+0          5          0          0          8148     */
