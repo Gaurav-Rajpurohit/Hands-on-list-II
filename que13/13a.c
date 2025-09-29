0d15b18ab4abdd9f60b46123bd1cf7fe2b86f737
Gaurav Rajpurohit <1762003rajpurohitgaurav@gmail.com>
2025-09-29 17:42:13 +0530

    First commit

---
+++ que13/13a.c
@@ -0,0 +1,34 @@
+/*
+============================================================================
+Name : 13a.c
+Author : Gaurav Rajpurohit
+Description : 13. Write two programs: first program is waiting to catch SIGSTOP signal, the second program  will send the signal (using kill system call). Find out whether the first program is able to catch  the signal or not. 
+
+Date: Sept 29th, 2025.
+============================================================================
+*/
+
+#include<stdio.h>
+#include<unistd.h>
+#include<signal.h>
+
+int main(){
+
+	printf("Program PID is : %d\n", getpid());
+	printf("Waiting for signal... \n");
+
+	pause();
+
+	printf("Signal Received from other program to stop this ongoing program\n");
+
+	return 0;
+}
+/*OUTPUT
+gaurav176@gaurav176-HP-Pavilion-Laptop-14-dv0xxx:~/Desktop/handsonlist2/que13$ cc 13a.c -o program1
+gaurav176@gaurav176-HP-Pavilion-Laptop-14-dv0xxx:~/Desktop/handsonlist2/que13$ cc 13b.c -o program2
+gaurav176@gaurav176-HP-Pavilion-Laptop-14-dv0xxx:~/Desktop/handsonlist2/que13$ ./program1
+Program PID is : 20057
+Waiting for signal... 
+
+[1]+  Stopped                 ./program1
+*/
