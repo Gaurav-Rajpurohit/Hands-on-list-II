f0ebcaf1dd7957703b9063f622da27b14bf291e6
Gaurav Rajpurohit <1762003rajpurohitgaurav@gmail.com>
2025-09-30 17:51:38 +0530

    third commit

--- que23/23.c
+++ que23/23.c
@@ -1,3 +1,14 @@
+/*
+============================================================================
+Name : 23.c
+Author : Gaurav Rajpurohit
+Description : 23. Write a program to print the maximum number of files can be opened within a process and  size of a pipe (circular buffer). 
+
+Date: Sept 30th, 2025.
+============================================================================
+*/
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
@@ -23,3 +34,9 @@ int main(){
 	close(fd[1]);
 	return 0;
 }
+/*OUTPUT
+gaurav176@gaurav176-HP-Pavilion-Laptop-14-dv0xxx:~/Desktop/handsonlist2/que23$ cc 23.c
+gaurav176@gaurav176-HP-Pavilion-Laptop-14-dv0xxx:~/Desktop/handsonlist2/que23$ ./a.out
+Max number of files that can be opened :1024
+pipe size(circular buffer): 65536 bytes
+*/
